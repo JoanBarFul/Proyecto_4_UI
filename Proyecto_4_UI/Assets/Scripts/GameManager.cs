af85f54d55f3268c65faab399b04f421de7602a8
JoanBarFul <jonbarful@gmail.com>
2021-11-30 10:46:19 +0100

    1

--- Proyecto_4_UI/Assets/Scripts/GameManager.cs
+++ Proyecto_4_UI/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using TMPro;
 
 
 public class GameManager : MonoBehaviour
@@ -12,35 +13,62 @@ public class GameManager : MonoBehaviour
     private float spaceSquares = 2.5f;
     private int numbersRows = 4;
     public bool gameOver;
-    private float timeSpawnRate = 2f;
-    public Vector3 ranSpawnPos;
-
+    public float timeSpawnRate = 1f;
+    private Vector3 ranSpawnPos;
+    public List<Vector3> targetPositions;
+    public TextMeshProUGUI scoreText;
+    public int score = 0;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI liveText;
+    public int live = 3;
+    public int missTarget;
+    public int totalmisses = 3;
     
+
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine("SpawnRandomTarget");
+        score = 0;
+        live = 3;
+        gameOverText.gameObject.SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = $"Score: {score}";
+        if (gameOver)
+        { gameOverText.gameObject.SetActive(true); }
+        liveText.text = $"Lives: X {live}";
+
+
     }
     public Vector3 RandomSpawnPosition()
     {
-        int ranInt = Random.Range(0, numbersRows);
-        float ranPosX = minX + ranInt * spaceSquares;
-        float ranPosY = minY + ranInt * spaceSquares;
+        int ranIntX = Random.Range(0, numbersRows);
+        int ranIntY = Random.Range(0, numbersRows);
+        float ranPosX = minX + ranIntX * spaceSquares;
+        float ranPosY = minY + ranIntY * spaceSquares;
 
         return new Vector3(ranPosX, ranPosY, 0);
     }
 
     private IEnumerator SpawnRandomTarget()
     {
-        yield return new WaitForSeconds(timeSpawnRate);
-        int ranIndex = Random.Range(0, targetPrefabs.Length);
-        ranSpawnPos = RandomSpawnPosition();
-        Instantiate(targetPrefabs[ranIndex], ranSpawnPos, targetPrefabs[ranIndex].transform.rotation);
+       while (!gameOver)
+        {
+            yield return new WaitForSeconds(timeSpawnRate);
+            int ranIndex = Random.Range(0, targetPrefabs.Length);
+            ranSpawnPos = RandomSpawnPosition();
+            while (targetPositions.Contains(ranSpawnPos))
+            {ranSpawnPos = RandomSpawnPosition(); }
+            Instantiate(targetPrefabs[ranIndex], ranSpawnPos, targetPrefabs[ranIndex].transform.rotation);
+            targetPositions.Add(ranSpawnPos);
+        }
     }
+  
+       
 }// para llamar corutina "StartCoroutine"
--- Proyecto_4_UI/Assets/Scripts/Target.cs
+++ Proyecto_4_UI/Assets/Scripts/Target.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    private float destroyTime = 2f;
+    public GameManager gameManagerScript;
+    private float destroyTime = 1f;
+    public ParticleSystem explosion;
+    
+   
+    
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, destroyTime);
+        gameManagerScript = FindObjectOfType<GameManager>();
+        
     }
 
     // Update is called once per frame
@@ -19,11 +26,26 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         if (gameObject.CompareTag("Good"))
-        { Destroy(gameObject); }
+        { Destroy(gameObject);
+            if (!gameManagerScript.gameOver)
+            { gameManagerScript.score = gameManagerScript.score + 1; }
+           
+        }
 
         else if (gameObject.CompareTag("Bad"))
         {
-            
+            Destroy(gameObject);
+            gameManagerScript.missTarget += 1;
+            if (gameManagerScript.missTarget > gameManagerScript.totalmisses)
+            { gameManagerScript.gameOver = true; }
+            if (!gameManagerScript.gameOver)
+            { gameManagerScript.live = gameManagerScript.live - 1; }
+
         }
+        Instantiate(explosion, transform.position, explosion.transform.rotation);
+    }
+    private void OnDestroy()
+    {
+        gameManagerScript.targetPositions.Remove(transform.position);
     }
 }
